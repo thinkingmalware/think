9d2f3c6034c79fb3901b40854efa0a3d206d4534
think3ngineering <44921791+thinkingmalware@users.noreply.github.com>
2021-04-11 14:18:31 +0900

    분류에 따른 크롤링

---
+++ f87309238673fadf.py
@@ -0,0 +1,23 @@
+﻿import requests
+
+response = requests.get('https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=100&sid2=268')
+
+html = response.text
+num = html.find('<ul class="type06_headline">')
+
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup.find('ul',class_="type06_headline").find_all('dl')
+
+for tag in tags:
+    a = tag.find('dt').find('a')
+    b = a.text.strip()
+        
+    if (b != "" and b != "동영상기사"):
+        print("제목: " + b)
+        print("링크: " + a.get('href'))
+        c = tag.find('span',class_="writing")
+        print("출처: " + c.text.strip())
+        print("")
